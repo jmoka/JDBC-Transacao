3c7cad1963d3a2ba9cfbc2271328f144243cf93b
teusiaqe <jmokatavares@gmail.com>
2025-07-25 23:23:13 -0300

    Segundo commit Tirando o Crud no Main

--- src/apication/app.java
+++ src/apication/app.java
@@ -1,5 +1,14 @@
 package apication;
 
+/*
+ * Exemplo de Transação:
+ * Uma transação é um conjunto de operações que devem ser executadas todas com
+ * sucesso ou nenhuma delas. Exemplo:
+ * Transferência bancária: subtrair de uma conta e adicionar em outra.
+ * Ambas as ações devem acontecer, ou nenhuma delas.
+ */
+
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -12,45 +21,7 @@ public class app {
 	public static void main(String[] args) {
 
 		RepositoryNivelUser nivelUser = new RepositoryNivelUser();
-
-		/* INSERIR */
-		// nivelUser.insert("ZECA", DB.getConectComPropertiesVariaveis());
-		// nivelUser.insert("LEPRA", DB.getConectComProperties());
-		// nivelUser.insert("luiz", DB.getCPNELConectComProperties());
-
-		/* DELETAR */
-		// nivelUser.daleteNivelId(1, DB.getConectComPropertiesVariaveis());
-		// nivelUser.daleteNivelId(2, DB.getConectComProperties());
-		// nivelUser.daleteNivelId(69, DB.getCPNELConectComProperties());
-
-		/* ATUALIZAR */
-		// nivelUser.updateNivelId(3, "hhh", DB.getConectComPropertiesVariaveis());
-		// nivelUser.updateNivelId(4, "lhhhhl", DB.getConectComProperties());
-		// nivelUser.updateNivelId(67, "ke", DB.getCPNELConectComProperties());
-
-		/* CONSULTAR */
-		// List<Nivel> list = nivelUser.queryAll(DB.getConectComPropertiesVariaveis());
-		// List<Nivel> list = nivelUser.queryAll(DB.getConectComProperties());
-		// List<Nivel> list = nivelUser.queryAll(DB.getCPNELConectComProperties());
-
-		/* LISTAR */
-		/*
-		System.out.println("=============");
-		for(Nivel n : list) {
-			System.out.println("Codigo : " + n.getIdNivel() );
-			System.out.println("Nome : " + n.getNomeNivel());
-			System.out.println("=============");
-		}
-		*/
-
-		/*
-		 * Exemplo de Transação:
-		 * Uma transação é um conjunto de operações que devem ser executadas todas com
-		 * sucesso ou nenhuma delas. Exemplo:
-		 * Transferência bancária: subtrair de uma conta e adicionar em outra.
-		 * Ambas as ações devem acontecer, ou nenhuma delas.
-		 */
-
+	
 		Boolean erro = true;
 
 		try (Connection conn = DB.getConectComProperties()) {
